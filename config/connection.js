bb47b0c6134cb10b9d30101b29f1b5cb1dbbffdb
ashay <albertshay888@gmail.com>
2019-02-22 17:30:23 -0800

    deployment

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,20 @@
 // *********************************************************************************
 
 // Dependencies
-var Sequelize = require("sequelize");
+var mysql = require('mysql');
+var connection;
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("feedr_db", "root", "password", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+if (process.env.JAWSDB_URL) {
+  connection = myysql.createConnection(process.env.JAWSBD_URL);
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'feedr_db'
+  });
+};
+
+connection.connect();
+module.exports = connection;
